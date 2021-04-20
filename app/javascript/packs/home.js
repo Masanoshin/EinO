fd4244865860b5c164c6a4759cde3e510ad82f48
shin <abcjpa@gmail.com>
2021-04-20 10:01:21 +0900

    メニューの改善

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,9 +1,29 @@
+let menu = document.querySelector('.menu');
 
+menu.addEventListener('click', comeout_menu);
+function comeout_menu() {
+    const menu = document.querySelector('.header-link');
+    const header = document.querySelector('.header');
+    if (header.classList.contains('header-height') === false){
+        header.classList.add('header-height');
+    } else {
+        console.log('tets');
+        header.classList.remove('header-height');
+    }
+
+    if (menu.classList.contains('header-link-comeout') === false){
+        menu.classList.add('header-link-comeout');
+    } else {
+        menu.classList.remove('header-link-comeout');
+    }
+}
 
 window.addEventListener('scroll', () => {
+
 	let fadein = document.querySelectorAll(".home_fadein");
 	let fadein_sec = document.querySelectorAll(".home_fadein_sec");
 	let home_opacity = document.querySelectorAll(".home_opacity");
+
 	fadein.forEach(el => {
 		let point = el.getBoundingClientRect().top;
 		if(550 > point){
